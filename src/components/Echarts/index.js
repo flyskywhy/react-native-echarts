5bb862a5d64e072fddea0757655d6072c5cd4c68
chenwenyu <chenwenyu@sys-in.com.cn>
2017-06-08 13:57:47 +0800

    增加web显示图表功能，增加安卓ios设置style功能

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -17,9 +17,37 @@ export default class App extends Component {
           ref="chart"
           scrollEnabled = {false}
           injectedJavaScript = {renderChart(this.props)}
-          style={{
+          style={[this.props.style,{
             height: this.props.height || 400,
-          }}
+          }]}
+          source={require('./tpl.html')}
+        />
+      </View>
+    );
+  }
+}
+import React, { Component } from 'react';
+import { WebView, View, StyleSheet } from 'react-native';
+import renderChart from './renderChart';
+import echarts from './echarts.min';
+
+export default class App extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.option !== this.props.option) {
+      this.refs.chart.reload();
+    }
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1, height: this.props.height || 400,}}>
+        <WebView
+          ref="chart"
+          scrollEnabled = {false}
+          injectedJavaScript = {renderChart(this.props)}
+          style={[this.props.style,{
+            height: this.props.height || 400,
+          }]}
           source={require('./tpl.html')}
         />
       </View>
